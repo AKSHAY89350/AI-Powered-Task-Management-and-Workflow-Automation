c681e147694547afd8937fe7d4e74685545cc5c6
Akshay Kumar <akshay89350@gmail.com>
2024-09-19 15:07:40 +0530

    integrate database

--- TaskManagementAPI/Task Management/obj/Debug/net6.0/Task Management.AssemblyInfo.cs
+++ TaskManagementAPI/Task Management/obj/Debug/net6.0/Task Management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task Management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2f8f760312cd594165c6fd6d5639f0036a7084")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task Management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task Management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
